925fd03e4bd5c8717500033379a183ec26b87eb2
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2022-09-19 17:12:44 -0500

    Update GitHub.jsx

--- client/src/components/GitHub.jsx
+++ client/src/components/GitHub.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { GITHUB, gitHubRedirectURL, path } from "../utils/gitHubCredentials.js";
+// import { GITHUB, gitHubRedirectURL, path } from "../utils/gitHubCredentials.js";
 import github from "../utils/images/github.png";
 
 export default function GitHub() { 
   return (
     <div style={{width: 262}}>
       <a
-        href={`https://github.com/login/oauth/authorize?client_id=${GITHUB}&redirect_uri=${gitHubRedirectURL}?path=${path}&scope=user:email`}
+        href={`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB}&redirect_uri=${process.env.gitHubRedirectURL}?path=${process.env.path}&scope=user:email`}
       >
         <img
           src={github}          
