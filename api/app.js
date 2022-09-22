7b54b821ff8439a31161db792cc4c094384f885f
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2022-09-21 20:41:42 -0500

    das

--- api/app.js
+++ api/app.js
@@ -8,30 +8,20 @@ const app = express();
 
 app.use(express.json()); // esta preparada para recibir info a través de un POST
 
-// // app.use(cors());
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://programando-ando-deploy.vercel.app"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
 // app.use(
 //   cors({
 //     origin: "https://programando-ando-deploy.vercel.app",
 //     credentials: true,
 //   })
 // );
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.use(
   fileUpload({
